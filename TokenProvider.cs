ef450b3725218257c5d23960047449ce143aa09e
Thanh Vu <thanhauco@gmail.com>
2026-03-24 01:41:39 -0700

    Add Azure Functions code generation and deployment documentation, .gitignore, project file, and token provider implementation

---
+++ TokenProvider.cs
@@ -0,0 +1,22 @@
+using Azure.Core;
+using Azure.Identity;
+
+namespace MCP_MI;
+
+public static class TokenProvider
+{
+    public static string GetIcmMcpApiToken()
+    {
+        // No pre-authorization needed - just acquire token and go!
+        var credential = new DefaultAzureCredential(
+            new DefaultAzureCredentialOptions
+            {
+                ManagedIdentityClientId = "3bc62a4d-a65e-48ed-af39-f70577ab184c"
+            });
+
+        AccessToken accessToken = credential.GetToken(
+            new TokenRequestContext(new[] { "api://icmmcpapi-ppe/.default" }));
+
+        return accessToken.Token;
+    }
+}
